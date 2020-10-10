6cdecb29e0a198052c7888a68fd9b87bc139ed38
Randika Rodrigo <subash.rodrigo@gmail.com>
2020-10-10 12:44:56 +0530

    Updated firebase admin initialization code.

--- functions/utils/firebaseAdmin.js
+++ functions/utils/firebaseAdmin.js
@@ -1,9 +1,7 @@
-const fb = require('firebase');
-const fbAdmin = require('firebase-admin');
 const fbConfig = require('../config/firebaseConfig');
+const firebase = require('firebase').initializeApp(fbConfig);
+const fbAdmin = require('firebase-admin').initializeApp(fbConfig);
 
-const firebase = fb.initializeApp(fbConfig);
 const db = fbAdmin.database();
-fbAdmin.initializeApp();
 
 module.exports = { firebase, fbAdmin, db, fbConfig };
