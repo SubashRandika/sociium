019bb82e36e8a0406ef0511721a0a37f2bea8569
Randika Rodrigo <subash.rodrigo@gmail.com>
2020-10-12 23:21:53 +0530

    Added get one post by it with comments details.

--- functions/routes/posts.js
+++ functions/routes/posts.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { db } = require('../utils/firebaseAdmin');
+const { data } = require('../utils/logger');
 const logger = require('../utils/logger');
 
+// create a new post
 router.post('/', (req, res) => {
 	logger.debug('POST - /posts reached.');
 
@@ -32,6 +34,7 @@ router.post('/', (req, res) => {
 		});
 });
 
+// get all posts
 router.get('/', (req, res) => {
 	logger.debug('GET - /posts path reached.');
 
@@ -58,6 +61,54 @@ router.get('/', (req, res) => {
 			res.status(500).send({
 				code: 500,
 				message: 'Unable to load the posts.'
+			});
+		});
+});
+
+// get one post by id
+router.get('/:postId', (req, res) => {
+	logger.debug('GET - /posts/:postId reached');
+
+	let postData = {};
+	const { postId } = req.params;
+	console.log(postId);
+
+	db.doc(`posts/${postId}`)
+		.get()
+		.then((doc) => {
+			if (!doc.exists) {
+				return res
+					.status(404)
+					.send({ code: 404, message: `Post: ${postId} not found` });
+			}
+
+			logger.debug(`getting post: ${postId} data.`);
+
+			postData = doc.data();
+			postData.postId = doc.id;
+
+			return db
+				.collection('comments')
+				.orderBy('createdAt', 'desc')
+				.where('postId', '==', postId)
+				.get();
+		})
+		.then((data) => {
+			logger.debug(`getting comments for post: ${postId}`);
+
+			postData.comments = [];
+			data.forEach((doc) => {
+				postData.comments.push(doc.data());
+			});
+
+			return res.status(200).send(postData);
+		})
+		.catch((err) => {
+			logger.error(`Unable to get post data due to: ${err}`);
+
+			res.status(500).send({
+				code: 500,
+				message: 'cannot get post data'
 			});
 		});
 });
